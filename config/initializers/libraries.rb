3c6049ec85b968e8f2648c37f41153e5897a29b3
Igal Koshevoy <igal@pragmaticraft.com>
2009-01-31 11:52:00 -0800

    Expanedded workaround for RedCloth.

--- config/initializers/libraries.rb
+++ config/initializers/libraries.rb
@@ -2,5 +2,9 @@
 require 'lib/ext_active_support_cache_filestore'
 
 # Local gems
-require 'RedCloth' # Make #textilize will work w/ RedCloth 4.1.x
-
+begin
+  # FIXME why is this causing exception some times, but not at others?
+  require 'RedCloth' # Make #textilize will work w/ RedCloth 4.1.x
+rescue LoadError
+  # Ignore
+end
